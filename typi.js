4c1cfd8a7a39f9099c20054241599033dfebd5fe
Sourjya Bose <sourjyabose@gmail.com>
2021-11-20 11:09:33 +0530

    Create typi.js

---
+++ typi.js
@@ -0,0 +1,12 @@
+function typi(writer){
+var str=writer;
+var l=str.length;
+i=1;
+var inter=setInterval(function(){
+if(i==l){clearInterval(inter);}
+
+var disp=document.getElementsByTagName("body")[0];
+disp.innerHTML=str.substr(0,i);
+i++;
+
+},150);}
